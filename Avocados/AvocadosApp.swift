144d662dff87a6079c294060502fc5b1258c7df0
NowFloats <prathap.reddy@nowfloats.com>
2023-09-27 00:00:48 +0530

    App icons and relavant image sets added to asset catalog

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Prathap Reddy on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
